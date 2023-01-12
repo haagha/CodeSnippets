4bb1e61a388e0b174e949cc7ad603828695eea05
Haider Agha <haagha@microsoft.com>
2023-01-12 11:41:04 -0500

    refactoring

--- CmdSeqCommonSubstring.py
+++ CmdSeqCommonSubstring.py
@@ -1,25 +1,46 @@
-# function for most common substring 
+# Description: Find the most common substring in a list of lists
+
+# Author: Haider Agha
+# Date: 12/12/2022
+# Version: 1.0
+# License: MIT
+
+# Usage: python CmdSeqCommonSubstring.py
+# Output: top 30 most common occuring sequences of 5 to 5 commands
+# Example: ('"storage blob upload","vm show","image create"', 2)
+
+# Note: This script is not optimized for performance. It is a proof of concept.
+
 import csv
 import time
 import re
+import sys
 
-list_of_list = []
-cmdlist = []
-counter = 0
-with open('30d30t.csv') as csvfile:
 
-    #data format = ['["account list","group show","storage account create"]']
-    list_of_list = (csv.reader(csvfile))
+# read csv file and convert to list of lists
+#data format = ['["account list","group show","storage account create"]']
+# remove brackets and split into list
+# cmdlist format = [['"account list"','"group show"','"storage account create"'], ['"account list2"','"group show2"','"storage account create2"']]
 
-    # remove brackets and split into list
-    # cmdlist format = [['"account list"','"group show"','"storage account create"'], ['"account list2"','"group show2"','"storage account create2"']]
-    for lines in list_of_list:
-        newline = re.sub('[\[\]]', '', lines[0])
-        newlist = newline.split(",")
-        cmdlist.append(newlist)
+def read_csv_file(filename):
+    list_of_list = []
+    cmdlist = []
+    with open(filename) as csvfile:
+        list_of_list = (csv.reader(csvfile))
+
+        # remove brackets and split into list
+        for lines in list_of_list:
+            newline = re.sub('[\[\]]', '', lines[0])
+            newlist = newline.split(",")
+            cmdlist.append(newlist)
         
-print("total sessions = " + str(len(cmdlist)))
+        # print number of sessions
+        print("total sessions = " + str(len(cmdlist)))
 
+        return cmdlist
+        
+
+# concatenate list data
 def concatenate_list_data(list):
     result= ' '
     for element in list:
@@ -27,19 +48,24 @@ def concatenate_list_data(list):
     return result
 
 
-# Create a set of all substrings in from all lists
-#create a copy of seq
 
+# find most common substring
+# iterate over each list in list of lists
+# iterate over each element in list
+# iterate over each element after i in list
+# if read command then keep iterating
+# if write command then add to write_seq
 def find_most_common_substring(list_of_list, min_key_len, max_key_len):
-    # First, we create a set of all substrings in from all lists
+
     substrings = dict()
-    for x in range(len(list_of_list)):        
+
+    for x in range(len(list_of_list)):       
         for i in range(len(list_of_list[x])-1):
             for j in range(i, len(list_of_list[x])-1):
                 
-                write_seq = []
+                write_seq = []                
                 temp_min_key = min_key_len
-                # iterate over each element in seq and if read then keep iterating
+
                 for k in range(j, j+temp_min_key):
                     if k+temp_min_key > len(list_of_list[x]):
                         break
@@ -51,7 +77,7 @@ def find_most_common_substring(list_of_list, min_key_len, max_key_len):
                         write_seq.append(command)
                         #print(command + " write command")
                 
-                #print(write_seq)
+                
                 if j+temp_min_key > len(list_of_list[x]):
                     break
                 if write_seq == []:
@@ -69,26 +95,14 @@ def find_most_common_substring(list_of_list, min_key_len, max_key_len):
                     substrings[seq] = 0
                 substrings[seq] += 1
     
-    #print(substrings)
     print("number of combinations = " + str(len(substrings.keys())))
-    #print("max(substrings.values()))
-    #sleep(600)
 
-    for key in list(substrings):
-        if not key.__contains__("vm"):
-            substrings.pop(key)
+    #for key in list(substrings):
+    #    if not key.__contains__("vm"):
+    #        substrings.pop(key)
 
-    #for kv in substrings.items():
-        #print(kv[0], kv[1])
-
-    # Then, we count the number of times each substring appears in both lists
     substring_counts = {}
-    #print(substrings)
 
-    # Finally, we return the substring with the highest count
-    #print(max(substrings, key=substrings.get))
-
-    # sort dictionary and print top 10
     print("top 30 most common occuring sequences of " + str(min_key_len) + " to " + str(max_key_len) + " commands")
     for kv in sorted(substrings.items(), key=lambda kv: kv[1], reverse=True)[:30]:
         print(kv[0], kv[1])
@@ -100,4 +114,12 @@ lister=[[''],['"storage blob upload","vm show","image create"'],
 ["storage blob upload2","vm show2","image create2","storage blob 21","vm show21","image create21","storage blob upload21","vm show21","image create21","storage blob upload22","vm show22","image create22"],
 ["storage blob upload3","vm show3","image create3"]]
 
-find_most_common_substring(cmdlist, 5, 5)
+# read command line arguments
+filename = sys.argv[1]
+min_key_len = int(sys.argv[2])
+max_key_len = int(sys.argv[3])
+
+cmdlist = read_csv_file(filename)
+
+# call function to find most common substring
+find_most_common_substring(cmdlist, min_key_len, max_key_len)
